8f884fd4a6471f919a39decc22e65bd75fcb0d22
joy <joy@gimjeongbeom-ui-MacBook-Pro-3.local>
2016-12-06 23:29:27 +0900

    file modified

--- PictureDiary/PictureDiary/UserInfo.h
+++ PictureDiary/PictureDiary/UserInfo.h
@@ -10,13 +10,15 @@
 
 @interface UserInfo : NSObject
 
+@property NSString *userEmail;
+@property NSString *userPassword;
 @property NSString *userId;
 @property NSString *userPass;
+
 @property NSString *userToken;
+
 @property NSDictionary *userData;
 
 + (instancetype)sharedUserInfo;
-- (void)saveUserEmail:(NSString *)email userPassword:(NSString *)password;
-- (void)saveLoginState:(BOOL)loginState;
 
 @end
--- PictureDiary/PictureDiary/UserInfo.m
+++ PictureDiary/PictureDiary/UserInfo.m
@@ -8,34 +8,19 @@
 
 #import "UserInfo.h"
 
-static NSString *userEmailKey = @"userEmail";
-static NSString *userPasswordKey = @"userPassword";
-static NSString *saveUserLoginKey = @"saveUserLogin";
-
 @implementation UserInfo
 
 + (instancetype)sharedUserInfo {
-    static UserInfo * instance = nil;
-    static dispatch_once_t token;
     
-    dispatch_once(&token, ^{
-        instance = [[self alloc] init];
+    static UserInfo *info = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        if (info == nil) {
+            info = [[UserInfo alloc] init];
+        }
     });
-    
-    return instance;
-}
-
-//아이디, 패스워드 저장
-- (void)saveUserEmail:(NSString *)email userPassword:(NSString *)password {
-
-    [[NSUserDefaults standardUserDefaults] setObject:email forKey:userEmailKey];
-    [[NSUserDefaults standardUserDefaults] setObject:password forKey:userPasswordKey];
-}
-
-// 로그인 상태 저장
-- (void)saveLoginState:(BOOL)loginState {
-    NSNumber *userLogin = [NSNumber numberWithBool:loginState];
-    [[NSUserDefaults standardUserDefaults] setObject:userLogin forKey:saveUserLoginKey];
+    return info;
 }
 
 @end
