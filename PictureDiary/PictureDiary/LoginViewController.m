f034b0696cc73bcff4376d429572378500a232f1
joy <joy@gimjeongbeom-ui-MacBook-Pro-3.local>
2016-12-08 17:03:05 +0900

    little change

--- PictureDiary/PictureDiary/LoginViewController.m
+++ PictureDiary/PictureDiary/LoginViewController.m
@@ -13,10 +13,8 @@
 #import "RequestObject.h"
 #import "AppDelegate.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKShareKit/FBSDKShareKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
-
-
+#import <FBSDKShareKit/FBSDKShareKit.h>
 
 @interface LoginViewController () <UITextFieldDelegate, UIScrollViewDelegate>
 
@@ -232,21 +230,12 @@
         alert = [UIAlertController alertControllerWithTitle:@"알림"
                                                     message:@"비밀번호를 입력하세요."
                                              preferredStyle:UIAlertControllerStyleAlert];
-<<<<<<< HEAD
     } else if ([RequestObject requestLoginData:email userPass:password] == NO) {
         // 등록되지 않은 이메일이거나 비밀번호가 틀린 경우
         alert = [UIAlertController alertControllerWithTitle:@"알림"
                                                     message:@"등록되지 않은 이메일이거나 이메일 또는 비밀번호를 잘못 입력하셨습니다."
                                              preferredStyle:UIAlertControllerStyleAlert];
-=======
->>>>>>> dev
     }
-//    else if () {
-//        // 등록되지 않은 이메일이거나 비밀번호가 틀린 경우
-//        alert = [UIAlertController alertControllerWithTitle:@"알림"
-//                                                    message:@"등록되지 않은 이메일입니다."
-//                                             preferredStyle:UIAlertControllerStyleAlert];
-//    }
     action = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleDefault handler:nil];
     [alert addAction:action];
     [self presentViewController:alert animated:YES completion:nil];
