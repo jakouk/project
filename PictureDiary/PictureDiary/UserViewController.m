1a7bcd37faa5e6bd39c9461db6af539aa479ba17
geonhui <cptcpt123@gmail.com>
2016-12-15 22:20:18 +0900

    modified userviewcontroller

--- PictureDiary/PictureDiary/UserViewController.m
+++ PictureDiary/PictureDiary/UserViewController.m
@@ -7,16 +7,177 @@
 //
 
 #import "UserViewController.h"
+#import "LoginViewController.h"
 
 @interface UserViewController ()
+<UITableViewDelegate, UITableViewDataSource>
 
+@property (strong, nonatomic) IBOutlet UITableView *userTableView;
+
+@property NSDictionary *userDataDic;
 @end
 
 @implementation UserViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self tableviewFrameSize];
+    
+    //유저정보와 로그아웃 노티피케이션
+    [RequestObject requestUserInfo];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(userDataRoad:)
+                                                 name:UserInfoNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(logoutViewChangeMethod:)
+                                                 name:LogoutNotification
+                                               object:nil];
+
+}
+
+//뷰가 화면에 나타난 후에 테이블뷰 델리게이트 실행
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    NSLog(@"viewdidappear");
+    [self.userTableView setDelegate:self];
+    [self.userTableView setDataSource:self];
+}
+
+#pragma mark - tableview setting
+//frame size
+- (void)tableviewFrameSize
+{
+    UIView *footerview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.userTableView.frame.size.width,
+                                                                  self.userTableView.frame.size.height)];
+    [footerview setBackgroundColor:[UIColor clearColor]];
+    [self.userTableView setTableFooterView:footerview];
+    
+}
+
+//session
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+//row
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+//cell
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+        [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        
+        //사용자 이름
+        if (indexPath.row == 0) {
+            [cell.imageView setImage:[UIImage imageNamed:@"Nameicon"]];
+            [cell.imageView setContentMode:UIViewContentModeScaleToFill];
+            [cell.textLabel setText:@"사용자 이름"];
+            [cell.detailTextLabel setText:[NSString stringWithFormat:@"%@",[self.userDataDic objectForKey:@"username"]]];
+        }
+        
+        //사다리 계정
+        if (indexPath.row == 1) {
+            [cell.imageView setImage:[UIImage imageNamed:@"Emailicon"]];
+            [cell.imageView setContentMode:UIViewContentModeScaleToFill];
+            [cell.textLabel setText:@"e-mail"];
+            [cell.detailTextLabel setText:[NSString stringWithFormat:@"%@",[self.userDataDic objectForKey:@"email"]]];
+        }
+        
+        //id
+        if (indexPath.row == 2) {
+            [cell.imageView setImage:[UIImage imageNamed:@"IDCardicon"]];
+            [cell.imageView setContentMode:UIViewContentModeScaleToFill];
+            [cell.textLabel setText:@"ID"];
+            [cell.detailTextLabel setText:[NSString stringWithFormat:@"%@",[self.userDataDic objectForKey:@"id"]]];
+        }
+    }
+    return cell;
+}
+
+//cell height
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80;
+}
+
+//header
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSString *headerString = [NSString stringWithFormat:@"프로필"];
+    return headerString;
+}
+
+//header height
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40;
+}
+
+#pragma mark - logout button
+- (IBAction)logoutButtonAction:(id)sender
+{
+    
+    //로그아웃 확인 얼럿
+    UIAlertController *logoutAlert = [UIAlertController alertControllerWithTitle:@"Log Out"
+                                                                         message:@"정말 로그아웃 하시겠어요?"
+                                                                  preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"확인"
+                                                 style:UIAlertActionStyleDefault
+                                               handler:^(UIAlertAction * _Nonnull action) {
+                                                   NSLog(@"로그아웃 허가");
+                                                   [RequestObject requestLogoutData];
+                                                   
+                                               }];
+    
+    UIAlertAction *no = [UIAlertAction actionWithTitle:@"취소"
+                                                 style:UIAlertActionStyleDestructive
+                                               handler:nil];
+    
+    [logoutAlert addAction:ok];
+    [logoutAlert addAction:no];
+    
+    [self presentViewController:logoutAlert animated:YES completion:nil];
+}
+
+#pragma mark - user data noti
+- (void)userDataRoad:(NSNotification *)noti
+{
+    NSDictionary *wordDic = noti.userInfo;
+    
+    self.userDataDic = wordDic;
+    NSLog(@"method in username : %@",[self.userDataDic objectForKey:@"username"]);
+    NSLog(@"method in email : %@",[self.userDataDic objectForKey:@"email"]);
+    NSLog(@"method in id : %@",[self.userDataDic objectForKey:@"id"]);
+    
+    [self.userTableView reloadData];
+}
+
+#pragma mark - logout noti
+- (void)logoutViewChangeMethod:(NSNotification *)noti {
+    
+    // MainTabBarController로 이동
+    UIStoryboard *storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *LoginView = [storyBoard instantiateInitialViewController];
+    UIApplication *application = [UIApplication sharedApplication];
+    UIWindow *window = [application.delegate window];
+    window.rootViewController = LoginView;
+    [window makeKeyAndVisible];
+    
 }
 
 - (void)didReceiveMemoryWarning {
